1f327e1bd3ff0e4de681a9a2562b19a2cd4db88a
Eli benson <eli.vasquezhdz@gmail.com>
2023-09-05 23:11:15 -0700

    adding button to remove last label

--- choices.py
+++ choices.py
@@ -1,18 +1,27 @@
 import tkinter as tk
 from tkinter import ttk
 import datetime as dt
+labels = []
 
 def add_label():
     choice1 = choices.get() + " " +  str(dt.datetime.now()).split(".")[0] + " " + name_entry.get()
     label1 = tk.Label(root, text=choice1)
     label1.pack()
+    labels.append(label1)
+
+def remove_last_label():
+    if labels:
+        last_label = labels.pop()
+        last_label.destroy()
+
+
 
 root = tk.Tk()
 root.title("ComboBox Choices")
 
 choices_label = tk.Label(root, text="Select Choice:")
 choices_label.pack()
-choices = ttk.Combobox(root, values=["A", "B", " C"])
+choices = ttk.Combobox(root, values=["A", "B", "C"])
 choices.pack()
 
 name_label = tk.Label(root ,text = " Name")
@@ -22,5 +31,8 @@ name_entry  = tk.Entry(root)#.grid(row = 0,column = 1)
 name_entry.pack()
 add_button = tk.Button(root, text="Add Labels", command=add_label)
 add_button.pack()
+
+remove_button = tk.Button(root, text="Remove Last Label", command=remove_last_label)
+remove_button.pack()
 
 root.mainloop()
